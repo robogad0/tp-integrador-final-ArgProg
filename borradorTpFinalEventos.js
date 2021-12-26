4718d7304084a9704354120f7645b4ded0dbfd87
Rocío <robogado8@gmail.com>
2021-12-26 18:40:57 -0300

    primer chttps://github.com/robogad0/tp-integrador-final-ArgProg.gii

---
+++ borradorTpFinalEventos.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+
+
+
+
+
+    $("#tablaExpLaboral").hide();
+
+    $("#expLaboral").click(function(){
+        $("#tablaExpLaboral").fadeToggle();
+    });
+
+
+    $("#tablaEducacion").hide();
+
+    $("#educacion").click(function(){
+        $("#tablaEducacion").fadeToggle();
+    });
+
+    $("#tareasTrabajo").hide();
+
+    $("#plusTrabajo").click(function(){
+        $("#tareasTrabajo").fadeToggle();
+    });
+
+    $(".botonLogin").mousemove(function(){
+        
+    });
+    
+
+
+
+
+
+
+})
+
+
+
